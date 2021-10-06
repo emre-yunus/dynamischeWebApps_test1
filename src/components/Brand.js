b15e117f67e1a7443003271c2d21a2a9501c7ec1
emre-yunus <r0664998@student.thomasmore.be>
2021-10-06 15:56:09 +0200

    oef 5

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -1,9 +1,12 @@
 export function Brands(props) {
     const {title, cars} = props;
+    const brands = [...new Set(cars.map(c => c.brand))];
+    const brandsWithNumbers = brands.map(b => ({name: b, numberOfCars: cars.reduce((counter, p) => (p.brand===b ? ++counter : counter), 0)}));
+    console.log(brandsWithNumbers)
     return (
         <div className="section">
             <h3>{title}</h3>
-            {cars.map(car => <Brand name={car.brand} key={car.name} />)}
+            {brandsWithNumbers.map(brand => <Brand name={brand.name} numberOfCars={brand.numberOfCars} key={brand.name} />)}
         </div>
     )
 }
@@ -11,9 +14,9 @@ export function Brands(props) {
 function Brand(props) {
     const {name, numberOfCars} = props;
     return (
-        <div className="section">
+        <div className="card big">
             <div>{name}</div>
-
+            <div>{numberOfCars}</div>
         </div>
     )
 }
