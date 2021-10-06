27462ed9c02b85329bd7b0efe495044c5f2af170
emre-yunus <r0664998@student.thomasmore.be>
2021-10-06 15:29:11 +0200

    oef 3

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import {CARS_DATA} from "./data/data";
 import {Cars} from "./components/Cars";
 import {ITEMS} from "./data/data";
-import {List} from "./components/List"
+import {List} from "./components/List";
+import {Brands} from "./components/Brand";
 
 function App() {
   return (
       <>
         <Cars title="Auto's" cars={CARS_DATA} />
         <List title="Een test lijst" items={ITEMS} />
+        <Brands title="Test merken" cars={CARS_DATA} />
       </>
   );
 }
--- src/components/Brand.js
+++ src/components/Brand.js
@@ -1,10 +1,19 @@
+export function Brands(props) {
+    const {title, cars} = props;
+    return (
+        <div className="section">
+            <h3>{title}</h3>
+            {cars.map(car => <Brand name={car.name} key={car.name} />)}
+        </div>
+    )
+}
 
 function Brand(props) {
     const {name, numberOfCars} = props;
     return (
-        <>
+        <div className="section">
             <div>{name}</div>
-            <div>{numberOfCars}</div>
-        </>
+
+        </div>
     )
 }
